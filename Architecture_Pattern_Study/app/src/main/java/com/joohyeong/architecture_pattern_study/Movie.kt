d0dcb623ab58a1de3f7419e678e2ddec1262b693
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-06-17 01:13:33 +0900

    feat: movie 데이터 클래스 생성

---
+++ Architecture_Pattern_Study/app/src/main/java/com/joohyeong/architecture_pattern_study/Movie.kt
@@ -0,0 +1,9 @@
+package com.joohyeong.architecture_pattern_study
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val posterUrl: String,
+    val audienceRating: String,
+    val runtime: String
+)
