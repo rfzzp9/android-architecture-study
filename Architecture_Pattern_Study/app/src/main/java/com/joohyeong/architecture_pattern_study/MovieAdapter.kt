1be5091fdf92c379a9255c4ca5437d5a57a101f6
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-06-18 20:37:48 +0900

    feat: Movie Adapter 생성

---
+++ Architecture_Pattern_Study/app/src/main/java/com/joohyeong/architecture_pattern_study/MovieAdapter.kt
@@ -0,0 +1,22 @@
+package com.joohyeong.architecture_pattern_study
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.joohyeong.architecture_pattern_study.domain.Movies
+
+class MovieAdapter(
+    private val movies: Movies
+) : RecyclerView.Adapter<MovieViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        return MovieViewHolder(
+            parent,
+            onMovieClick = {}
+        )
+    }
+
+    override fun getItemCount(): Int = movies.getMovies().size
+
+    override fun onBindViewHolder(viewHolder: MovieViewHolder, position: Int) {
+        viewHolder.bind(movies.getMovies()[position])
+    }
+}
