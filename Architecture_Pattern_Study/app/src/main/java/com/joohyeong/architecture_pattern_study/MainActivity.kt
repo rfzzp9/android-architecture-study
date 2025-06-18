9d0282ef4912e3bb0ada87168f69c2c64c6081cc
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-06-18 20:38:29 +0900

    feat: 데이터 로딩 및 movie adapter 연결

--- Architecture_Pattern_Study/app/src/main/java/com/joohyeong/architecture_pattern_study/MainActivity.kt
+++ Architecture_Pattern_Study/app/src/main/java/com/joohyeong/architecture_pattern_study/MainActivity.kt
@@ -6,17 +6,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.joohyeong.architecture_pattern_study.databinding.ActivityMainBinding
+import com.joohyeong.architecture_pattern_study.domain.MovieRepository
+import com.joohyeong.architecture_pattern_study.domain.Movies
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val movieRepository = MovieRepository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         applySystemBarsPadding()
+
+        val movies = movieRepository.fetchMovies()
+        initMovieAdapter(movies)
     }
 
     private fun applySystemBarsPadding() {
@@ -25,5 +32,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun initMovieAdapter(movies: Movies) {
+        binding.recyclerMovie.adapter = MovieAdapter(
+            movies = movies,
+        )
     }
 }
