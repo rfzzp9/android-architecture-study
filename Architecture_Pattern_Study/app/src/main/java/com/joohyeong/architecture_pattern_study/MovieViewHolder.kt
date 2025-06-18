7ba17f1e77444f106b5bfef28f6baf738163c51c
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-06-18 20:37:39 +0900

    feat: Movie ViewHolder 생성

---
+++ Architecture_Pattern_Study/app/src/main/java/com/joohyeong/architecture_pattern_study/MovieViewHolder.kt
@@ -0,0 +1,31 @@
+package com.joohyeong.architecture_pattern_study
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.joohyeong.architecture_pattern_study.databinding.ItemMovieBinding
+import com.joohyeong.architecture_pattern_study.domain.Movie
+
+class MovieViewHolder(
+    parent: ViewGroup,
+    private val onMovieClick: (position: Int) -> Unit,
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context)
+        .inflate(R.layout.item_movie, parent, false)
+) {
+    private val binding = ItemMovieBinding.bind(itemView)
+
+    init {
+        binding.root.setOnClickListener {
+            onMovieClick(adapterPosition)
+        }
+    }
+
+    fun bind(movie: Movie) {
+        with(binding) {
+            textMovieTitle.text = movie.title
+            textMovieAudienceRating.text = movie.audienceRating
+            textMovieRuntime.text = movie.runtime
+        }
+    }
+}
