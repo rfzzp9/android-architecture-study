6ca66f781270a6650a85301d1962b385e35a0162
chaewon gwak <rfzzp9@gmail.com>
2025-06-19 23:40:41 +0900

    영화상세정보 API를 활용한 MVC 패턴 코드 작성_3
    1. 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.devtools.ksp)
+    alias(libs.plugins.plugin.serialization)
 }
 
 android {
@@ -33,10 +35,19 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp3.logging.interceptor)
+    implementation(libs.kotlin.serialization.json)
+    implementation(libs.glide)
+    implementation(libs.converter)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
