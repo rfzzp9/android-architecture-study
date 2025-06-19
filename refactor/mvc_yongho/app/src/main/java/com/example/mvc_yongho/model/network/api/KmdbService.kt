eaed03c7359d4876415f047c91501686e3a51489
yongho2157 <studyyongho2157@gmail.com>
2025-06-19 22:02:21 +0900

    feat: mvc pattern 적용

---
+++ refactor/mvc_yongho/app/src/main/java/com/example/mvc_yongho/model/network/api/KmdbService.kt
@@ -0,0 +1,16 @@
+package com.example.mvc_yongho.model.network.api
+
+import com.example.mvc_yongho.BuildConfig
+import com.example.mvc_yongho.model.data.KmdbResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface KmdbService {
+    @GET("openapi-data2/wisenut/search_api/search_json2.jsp")
+    suspend fun searchMovies(
+        @Query("collection") collection: String = "kmdb_new2",
+        @Query("detail") detail: String = "N",
+        @Query("ServiceKey") serviceKey: String = BuildConfig.SERVICE_KEY,
+        @Query("title") title: String
+    ): KmdbResponse
+}
