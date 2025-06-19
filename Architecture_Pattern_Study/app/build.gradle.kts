f3371fc22b73c6be89bd4162f9dd5d244a7a35cd
kwakjoohyeong <rhkrwngud445@gmail.com>
2025-06-19 23:37:17 +0900

    feat: kotlin-serialization 의존성 추가

--- Architecture_Pattern_Study/app/build.gradle.kts
+++ Architecture_Pattern_Study/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.glide)
     implementation(libs.retrofit)
+    implementation(libs.kotlinx.serialization.json)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
