bc03f1bcda300231b5f60f58d10e998de5b20436
chaewon gwak <rfzzp9@gmail.com>
2025-07-05 22:50:49 +0900

    refactor: ResponseDTO -> Response 네이밍 변경

--- app/src/main/java/com/example/mvc_project/data/dto/ResponseDTO.kt
+++ app/src/main/java/com/example/mvc_project/data/dto/ResponseDTO.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ResponseDTO(
+data class Response(
     @SerialName("Query")
     val query: String? = null,
     @SerialName("KMAQuery")
