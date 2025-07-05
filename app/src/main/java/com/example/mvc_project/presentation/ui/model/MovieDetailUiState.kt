30326b1192297d5711cf975b406c0f80010dad20
chaewon gwak <rfzzp9@gmail.com>
2025-07-05 23:22:38 +0900

    변경 없이 커밋

--- app/src/main/java/com/example/mvc_project/presentation/ui/model/MovieDetailUiState.kt
+++ app/src/main/java/com/example/mvc_project/presentation/ui/model/MovieDetailUiState.kt
@@ -1,2 +1,12 @@
-package com.example.mvc_project.presentation.ui.model 
+package com.example.mvc_project.presentation.ui.model
 
+data class MovieDetailUiState(
+    val moviePoster: String = "",
+    val movieName: String = "",
+    val movieRunningTime: String = "",
+    val movieGrade: String = "",
+    val actorName: String = "",
+    val director: String = "",
+    val plotText: String = "",
+    val prodYear: String = "",
+)
