12455599f9ff7d8c653f3235f046dbd287d49c6b
chaewon gwak <rfzzp9@gmail.com>
2025-07-05 22:37:57 +0900

    refactor: 메인화면의 데이터 상태를 영화상세 화면 데이터로 전환하는 mapper 파일 추가

---
+++ app/src/main/java/com/example/mvc_project/presentation/ui/main/mapper/MovieDetailUiMapper.kt
@@ -0,0 +1,15 @@
+package com.example.mvc_project.presentation.ui.main.mapper
+
+import com.example.mvc_project.domain.model.MovieDetailUiState
+import com.example.mvc_project.domain.model.MovieUiState
+
+fun MovieUiState.toMovieDetailUiState() = MovieDetailUiState(
+    moviePoster = this.moviePoster.orEmpty(),
+    movieName = this.movieName.orEmpty(),
+    movieRunningTime = this.movieRunningTime.orEmpty(),
+    movieGrade = this.movieGrade.orEmpty(),
+    actorName = this.actorName.orEmpty(),
+    director = this.director.orEmpty(),
+    plotText = this.plotText.orEmpty(),
+    prodYear = this.prodYear.orEmpty()
+)
