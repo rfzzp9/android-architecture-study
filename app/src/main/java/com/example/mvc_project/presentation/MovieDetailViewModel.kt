58e4daaf261e116b41ba30b60a79c92bec40ac9b
chaewon gwak <rfzzp9@gmail.com>
2025-08-03 15:20:45 +0900

    refactor: viewModel 데이터 검증 로직 삭제

--- app/src/main/java/com/example/mvc_project/presentation/MovieDetailViewModel.kt
+++ app/src/main/java/com/example/mvc_project/presentation/MovieDetailViewModel.kt
@@ -16,28 +16,20 @@ class MovieDetailViewModel : ViewModel() {
     fun setMovieData(movie: MovieUiState) {
         viewModelScope.launch {
             try {
-                if (isValidMovieData(movie)) {
-                    _movieDetailUiState.value = MovieDetailUiState(
-                        moviePoster = movie.moviePoster ?: "",
-                        movieName = movie.movieName ?: "",
-                        movieRunningTime = movie.movieRunningTime ?: "",
-                        movieGrade = movie.movieGrade ?: "",
-                        actorName = movie.actorName ?: "",
-                        director = movie.director ?: "",
-                        plotText = movie.plotText ?: "",
-                        prodYear = movie.prodYear ?: ""
-                    )
-                } else {
-                    _movieDetailUiState.value = MovieDetailUiState()  // 유효하지 않은 데이터일 경우 빈 상태로 설정
-                }
+                _movieDetailUiState.value = MovieDetailUiState(
+                    moviePoster = movie.moviePoster ?: "",
+                    movieName = movie.movieName ?: "",
+                    movieRunningTime = movie.movieRunningTime ?: "",
+                    movieGrade = movie.movieGrade ?: "",
+                    actorName = movie.actorName ?: "",
+                    director = movie.director ?: "",
+                    plotText = movie.plotText ?: "",
+                    prodYear = movie.prodYear ?: ""
+                )
             } catch (e: Exception) {
                 _movieDetailUiState.value = MovieDetailUiState()      // 예외 발생 시 빈 상태로 설정
             }
         }
     }
 
-    // 영화 제목으로 유효한 데이터인지 확인
-    private fun isValidMovieData(movie: MovieUiState): Boolean {
-        return !movie.movieName.isNullOrBlank()
-    }
 }
